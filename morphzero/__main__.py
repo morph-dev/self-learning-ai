795c7d4206c73615b2c3ea9ebfbecf969aea5b5d
morph <82043364+morph-dev@users.noreply.github.com>
2021-05-27 21:49:41 +0300

    Fix __main__.

--- morphzero/__main__.py
+++ morphzero/__main__.py
@@ -2,68 +2,8 @@ import os.path
 
 from morphzero.game.genericgomoku import GenericGomokuRules, GenericGomokuGameEngine
 from morphzero.training.hashgomoku import HashGomokuTrainer, HashGomokuModel
-from morphzero.ui.genericgomokuui import GenericGomokuApp
-
-
-def play_human_vs_human():
-    rules = GenericGomokuRules(board_size=(3, 3), goal=3, first_player_name="Iks", second_player_name="Oks")
-    engine = GenericGomokuGameEngine(rules)
-    state = engine.new_game()
-    print(state)
-    while not state.is_game_over:
-        while True:
-            move_str = input("Enter move to play: ")
-            move = tuple(int(x) for x in move_str.split())
-            if engine.is_move_playable(state, move):
-                break
-            print(f"Move {move} is not playable move ({list(engine.playable_moves(state))}).")
-        state = engine.play_move(state, move)
-        print(state)
-    print(f"Game is over! Result: {state.result}")
-
-
-def play_human_vs_ai(path, debug):
-    rules = GenericGomokuRules(board_size=(3, 3), goal=3, first_player_name="Iks", second_player_name="Oks")
-    engine = GenericGomokuGameEngine(rules)
-    model = HashGomokuModel.deserialize(path)
-
-    def play_human(state):
-        print(state)
-        while True:
-            move_str = input("Enter move to play: ")
-            move = tuple(int(x) for x in move_str.split())
-            if engine.is_move_playable(state, move):
-                return move
-            print(f"Move {move} is not playable move ({list(engine.playable_moves(state))}).")
-
-    def play_ai(state):
-        print(state)
-        if debug:
-            for move in engine.playable_moves(state):
-                print(move, model.state_policy.get(engine.play_move(state, move), None))
-        move = model.play_move(engine, state)
-        print(f"Ai plays {move}")
-        return move
-
-    play_as = int(input("Play as:\n1. Iks\n2. Oks\n"))
-    if play_as == 1:
-        current_player = play_human
-        other_player = play_ai
-    elif play_as == 2:
-        current_player = play_ai
-        other_player = play_human
-    else:
-        print("Unknown input")
-        return
-
-    game_state = engine.new_game()
-    while not game_state.is_game_over:
-        played_move = current_player(game_state)
-        game_state = engine.play_move(game_state, played_move)
-        current_player, other_player = other_player, current_player
-
-    print(game_state)
-    print(f"Game is over! Result: {game_state.result}")
+from ui.gameconfigapp import RulesConfig, GameConfigApp, ModelConfig
+from ui.genericgomokuui import GenericGomokuApp
 
 
 def train_model(path, iterations, learning_rate, exploration_rate):
@@ -72,7 +12,7 @@ def train_model(path, iterations, learning_rate, exploration_rate):
     else:
         model = HashGomokuModel()
 
-    rules = GenericGomokuRules(board_size=(3, 3), goal=3, first_player_name="Iks", second_player_name="Oks")
+    rules = GenericGomokuRules(board_size=(3, 3), goal=3)
     engine = GenericGomokuGameEngine(rules)
     trainer = HashGomokuTrainer(engine, learning_rate=learning_rate, exploration_rate=exploration_rate, model=model)
 
@@ -91,16 +31,7 @@ def train_model(path, iterations, learning_rate, exploration_rate):
     model.serialize(path)
 
 
-def gomoku_ui():
-    rules = GenericGomokuRules(board_size=(3, 3), goal=3, first_player_name="Iks", second_player_name="Oks")
-    # rules = GenericGomokuRules(board_size=(15,15), goal=5, first_player_name="Iks", second_player_name="Oks")
-    app = GenericGomokuApp(rules)
-    app.mainloop()
-
-
 def main():
-    # playHumanVsHuman()
-
     # train_config = {
     #     "iterations": 10000,
     #     "learning_rate": 0.3,
@@ -109,13 +40,42 @@ def main():
     # path_format = "./models/hash_gomoku_i{iterations}_lr{learning_rate}_er{exploration_rate}.model"
     # trainModel(path_format.format(**train_config), **train_config)
 
-    play_human_vs_ai(
-        # "./models/hash_gomoku_i100000_lr0.1_er0.3.model",
-        "./models/hash_gomoku_i10000_lr0.3_er0.2.model",
-        debug=True,
-    )
+    rules_config = [
+        RulesConfig(
+            "TicTacToe",
+            GenericGomokuRules(board_size=(3, 3), goal=3),
+            [
+                ModelConfig("Human", None, None),
+                ModelConfig("i100000_lr0.1_er0.3",
+                            "./models/hash_gomoku_i100000_lr0.1_er0.3.model",
+                            HashGomokuModel.deserialize),
+                ModelConfig("i10000_lr0.3_er0.2",
+                            "./models/hash_gomoku_i10000_lr0.3_er0.2.model",
+                            HashGomokuModel.deserialize),
+            ]
+        ),
+        RulesConfig(
+            "Gomoku (15x15)",
+            GenericGomokuRules(board_size=(15, 15), goal=5),
+            [ModelConfig("Human", None, None)]
+        ),
+        RulesConfig(
+            "Gomoku (19x19)",
+            GenericGomokuRules(board_size=(19, 19), goal=5),
+            [ModelConfig("Human", None, None)]
+        ),
+    ]
 
-    # gomoku_ui()
+    app = GameConfigApp(rules_config)
+    app.mainloop()
+
+    game_config = app.game_config
+    if game_config is None:
+        return
+    print(game_config)
+
+    app = GenericGomokuApp(game_config)
+    app.mainloop()
 
 
 if __name__ == "__main__":
